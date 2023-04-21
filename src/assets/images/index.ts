6eebd44fc7a2ec401e74b1700d28ce10a81e7d86
Ruslan Fedchenko <fedchenko@lanit.ru>
2023-04-21 13:00:52 +0300

    Add man images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,23 @@
+import AngryMan from "./angry-man.png";
 import AngryWoman from "./angry-woman.png";
+import HappyMan from "./happy-man.png";
 import HappyWoman from "./happy-woman.png";
+import NervousMan from "./nervous-man.png";
 import NervousWoman from "./nervous-woman.png";
+import NeutralMan from "./neutral-man.png";
 import NeutralWoman from "./neutral-woman.png";
+import SatisfiedMan from "./satisfied-man.png";
 import SatisfiedWoman from "./satisfied-woman.png";
 
-export { AngryWoman, HappyWoman, NervousWoman, NeutralWoman, SatisfiedWoman };
+export {
+  AngryMan,
+  AngryWoman,
+  HappyMan,
+  HappyWoman,
+  NervousMan,
+  NervousWoman,
+  NeutralMan,
+  NeutralWoman,
+  SatisfiedMan,
+  SatisfiedWoman,
+};
